409494dd160ea6a2f93bc72411d2515f5f09bf87
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2016-04-08 15:41:26 +0200

    Changed menu hover color. Added dummydata sql script.

--- alpineclub.py
+++ alpineclub.py
@@ -34,6 +34,9 @@ def init_db():
     with app.open_resource('schema.sql', mode='r') as f:
         db.cursor().executescript(f.read())
     db.commit()
+    with app.open_resource('dummydata.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
 
 
 def put_db(table, values):
---
+++ dummydata.sql
@@ -0,0 +1,5 @@
+INSERT INTO ski_packs VALUES ("0", "Yngstemann", "Denne pakken egner seg godt til de aller yngste, alder 3-6 år.", "100", "300", "500");
+INSERT INTO ski_packs VALUES ("1", "De litt eldre", "Denne pakken er rettet mot barn i alder 7-12 år.", "150", "400", "800");
+INSERT INTO ski_packs VALUES ("2", "Tenåringen", "Denne pakken er rettet mot tenåringer i alder 13-17", "250", "600", "1000");
+INSERT INTO ski_packs VALUES ("3", "Twin-tip", "Denne pakken er rettet mot alle i alder 18+. Pakken kommer bare med twin-tip.", "250", "600", "1000");
+INSERT INTO ski_packs VALUES ("4", "Cross", "Denne pakken er rettet mot alle i alder 18+. Pakken kommer bare med criss-ski.", "250", "600", "1000");
